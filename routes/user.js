11cd2b734a725602d8b3693cd3a31c7940a54de7
YashTitarmare <titarmareyash6@gmail.com>
2026-03-09 17:45:06 +0530

    Initial adding the perviouse files

---
+++ routes/user.js
@@ -0,0 +1,22 @@
+const express=require("express");
+const router=express.Router();
+
+const {handleUserRegister,handleUserLogin,handleLogout}=require("../controllers/user");
+
+
+// Render register page
+
+
+
+// for the from happinesss
+router.post("/register",handleUserRegister);
+
+//router.post("/login",login);
+
+router.post("/login",handleUserLogin)
+
+
+router.get("/logout", handleLogout);
+
+
+module.exports=router;
---
+++ service/auth.js
@@ -0,0 +1,47 @@
+// import { nanoid } from "nanoid";
+
+const jwt = require("jsonwebtoken");
+ const secret="Yash1234";
+
+// const secret=nanoid();
+
+// console.log(secret);
+
+function setUser(user){
+
+    // return jwt.sign(user,secret);
+
+return jwt.sign({
+    _id :user._id,
+    email:user.email,
+    name:user.name,
+
+    // 3 red adding the role in the payload for the verfiay 
+
+    role : user.role,
+    }, 
+    secret)
+};
+
+
+
+function getUser(token){
+
+    // console.log("TOKEN RECEIVED:", token);
+    if(!token) return null;
+    return jwt.verify(token, secret);
+}
+
+module.exports={
+    setUser,
+    getUser,
+}
+
+
+
+
+
+
+
+
+
